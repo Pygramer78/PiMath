166f62b0a019f7d70637f4879f046ebe50c5aa11
Pygramer78 <ayuskyap@gmail.com>
2025-06-02 21:29:03 +0200

    First file added

---
+++ primes.py
@@ -0,0 +1,17 @@
+class Primes:
+    pass
+
+def is_prime(a):
+    pass
+
+def generate_primes():
+    pass
+
+def factorial_decompose(a):
+    pass
+
+def mcm(a, b):
+    pass
+
+def mcd(a, b):
+    pass
