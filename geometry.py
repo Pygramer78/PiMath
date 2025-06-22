9fd40d423d15f67ae53db0854d88ba481b79ae5e
Pygramer78 <ayuskyap@gmail.com>
2025-06-22 13:16:42 +0200

    Made many classes

--- geometry.py
+++ geometry.py
@@ -3,6 +3,11 @@ from turtle import *
 from colors import Color
 from number import PI
 import time as tm
+from math import sqrt
+
+def pythagorean_theorem(c1, c2):
+    return sqrt(c1*2 + c2*2)
+
 class Coordinates:
     def __init__(self, x, y):
         self.x = x
@@ -118,9 +123,9 @@ class Trapeze:
         return (self.bd + self.sd)*self.h / 2
     
     def perimeter(self):
-        return "In development."
+        return self.bd + self.sd + self.h
     
-class Triangle:
+class Triangle: # ONLY equilateral triangles
     def __init__(self, alture, base):
         self.h = alture
         self.b = base
@@ -132,4 +137,146 @@ class Triangle:
         return self.h*self.b / 2
     
     def perimeter(self):
-        return "Development."
+        return self.b*3
+
+class Custom:
+    def __init__(self, apothem, sides, sidelen):
+        self.ap = apothem
+        self.sides = sides
+        self.sidelen = sidelen
+    
+    def __repr__(self):
+        return f"Custom. \nApothem: {self.ap}\nSides: {self.sides}\nSide length: {self.sidelen}"
+    
+    def perimeter(self):
+        return self.sidelen * self.sides
+
+    def area(self):
+        perimeter = self.perimeter()
+        return self.ap * perimeter / 2
+
+
+class Dodecagon:
+    def __init__(self, apothem, sidelen):
+        self.ap = apothem
+        self.sidelen = sidelen
+    
+    def __repr__(self):
+        return f"Dodecagon.\nApothem: {self.ap}\nSide length: {self.sidelen}"
+    
+    def perimeter(self):
+        return self.sidelen * 12
+    
+    def area(self):
+        perimeter = self.perimeter()
+        return self.ap * perimeter / 2
+
+
+class Hendecagon:
+    def __init__(self, apothem, sidelen):
+        self.ap = apothem
+        self.sidelen = sidelen
+    
+    def __repr__(self):
+        return f"Hendecagon.\nApothem: {self.ap}\nSide length: {self.sidelen}"
+    
+    def perimeter(self):
+        return self.sidelen * 11
+    
+    def area(self):
+        perimeter = self.perimeter()
+        return self.ap * perimeter / 2
+
+class Decagon:
+    def __init__(self, apothem, sidelen):
+        self.ap = apothem
+        self.sidelen = sidelen
+    
+    def __repr__(self):
+        return f"Decagon.\nApothem: {self.ap}\nSide length: {self.sidelen}"
+    
+    def perimeter(self):
+        return self.sidelen * 10
+    
+    def area(self):
+        perimeter = self.perimeter()
+        return self.ap * perimeter / 2
+
+class Enneagon:
+    def __init__(self, apothem, sidelen):
+        self.ap = apothem
+        self.sidelen = sidelen
+    
+    def __repr__(self):
+        return f"Enneagon. \nApothem: {self.ap}\nSide length: {self.sidelen}"
+    
+    def perimeter(self):
+        return self.sidelen * 9
+    
+    def area(self):
+        perimeter = self.perimeter()
+        return self.ap * perimeter / 2
+
+
+
+class Octogon:
+    def __init__(self, apothem, sidelen):
+        self.ap = apothem
+        self.sidelen = sidelen
+    
+    def __repr__(self):
+        return f"Octogon. \nApothem: {self.ap}\nSide length: {self.sidelen}"
+    
+    def perimeter(self):
+        return self.sidelen * 8 # Since has been already definedç
+    
+    def area(self):
+        perimeter = self.perimeter()
+        return self.ap * perimeter / 2
+
+class Heptagon:
+    def __init__(self, apothem, sidelen):
+        self.ap = apothem
+        self.sidelen = sidelen
+    
+    def __repr__(self):
+        return f"Heptagon.\nApothem: {self.ap}\nSide length: {self.sidelen}"
+    
+    def perimeter(self):
+        return self.sidelen * 7
+    
+    def area(self):
+        perimeter = self.perimeter()
+        return self.ap * perimeter / 2
+
+
+class Hexagon:
+    def __init__(self, apothem, sidelen):
+        self.ap = apothem
+        self.sidelen = sidelen
+    
+    def __repr__(self):
+        return f"Hexagon.\nApothem: {self.ap}\nSide length: {self.sidelen}"
+    
+    def perimeter(self):
+        return self.sidelen * 6
+
+    def area(self):
+        perimeter = self.perimeter()
+        return self.ap * perimeter / 2
+
+class Pentagon:
+    def __init__(self, apothem, sidelen):
+        self.ap = apothem
+        self.sidelen = sidelen
+    
+    def __repr__(self):
+        return f"Pentagon.\nApothem: {self.ap}\nSide length: {self.sidelen}"
+
+    def perimeter(self):
+        return self.sidelen * 5
+    
+    def area(self):
+        perimeter = self.perimeter()
+        return self.ap * perimeter / 2
+
